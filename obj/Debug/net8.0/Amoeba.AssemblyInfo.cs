725401f3a374913cfb94e15eed1998cd38b20f31
rincedal <94070451+Diomuzan247@users.noreply.github.com>
2023-08-29 14:00:21 +0200

    Recreated ASP.NET project

--- obj/Debug/net8.0/Amoeba.AssemblyInfo.cs
+++ obj/Debug/net8.0/Amoeba.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Amoeba")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d02f06ae5da51bbdf8ae4a45605ca422c447e0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Amoeba")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Amoeba")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

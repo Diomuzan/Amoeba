00c5ce200c63e5b99436796f05604550bffdaf68
rincedal <94070451+Diomuzan247@users.noreply.github.com>
2023-09-22 14:19:44 +0200

    Finished writing Amoeba

--- obj/Debug/net8.0/Amoeba.AssemblyInfo.cs
+++ obj/Debug/net8.0/Amoeba.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Amoeba")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8586f9ccccd9013c73a5cae8898583df08dbc019")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5bb542c0e03cdf8a2440007b093e0a4cb24bc60")]
 [assembly: System.Reflection.AssemblyProductAttribute("Amoeba")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Amoeba")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

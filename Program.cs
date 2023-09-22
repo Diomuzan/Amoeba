6f4383f874b6b141c0df2c3ab553d1b7ba0df06e
rincedal <94070451+Diomuzan247@users.noreply.github.com>
2023-09-22 16:51:45 +0200

    Added files

--- Program.cs
+++ Program.cs
@@ -31,19 +31,15 @@ Console.WriteLine((coin.Next(0, 2) == 0) ? "heads" : "tails");
 string permission = "Admin|Manager";
 int level = 55;
 
-string permission = "Admin|Manager";
-int level = 55;
-
 if (permission.Contains("Admin") && level > 55) {
     Console.WriteLine("Welcome, Super Admin user!");
-} else if (permission.Contains("Admin") && level <= 55){
+} else if (permission.Contains("Admin") && level <= 55) {
     Console.WriteLine("Welcome, Admin user!");
 } else if (permission.Contains("Manager") && level >= 20) {
     Console.WriteLine("Contact an Admin for access.");
 } else if (permission.Contains("Manager") && level < 20) {
     Console.WriteLine("You do not have sufficient privileges.");
-}
-else {
+} else {
     Console.WriteLine("You do not have sufficient privileges.");
 }
 
